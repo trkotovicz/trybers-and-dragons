1161cd270ec4c63a6b98503eee323e8fdf7020ec
Thais Kotovicz <thais_kotovicz@hotmail.com>
2022-08-10 15:11:08 -0300

    feat: [req 11] Cria a classe de Batalha "PVE"

---
+++ src/Battle/PVE.ts
@@ -0,0 +1,18 @@
+import Fighter, { SimpleFighter } from '../Fighter';
+import Battle from './Battle';
+
+export default class PVE extends Battle {
+  constructor(player: Fighter, private monsters: Fighter[] | SimpleFighter[]) {
+    super(player);
+  }
+
+  fight(): number {
+    while (
+      this.player.lifePoints >= 0
+      || this.monsters.every((monster) => monster.lifePoints >= 0)) {
+      this.monsters.forEach((monster) => this.player.attack(monster));
+      this.monsters.forEach((monster) => monster.attack(this.player));
+    }
+    return this.player.lifePoints === -1 ? -1 : 1;
+  }
+}
