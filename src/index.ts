5d42e4ece7e20e7600208ab0d529493b735f6b89
Thais Kotovicz <thais_kotovicz@hotmail.com>
2022-08-10 15:44:22 -0300

    feat: [req 13] Cria as instâncias do jogo no index.ts

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import Character from './Character';
+import Dragon from './Dragon';
+import Monster from './Monster';
+import PVE from './Battle/PVE';
+import Battle from './Battle';
+
+const player1 = new Character('player1');
+const player2 = new Character('player2');
+const player3 = new Character('player3');
+
+player1.levelUp();
+player1.levelUp();
+player1.levelUp();
+
+const monster1 = new Monster();
+const monster2 = new Dragon();
+
+const pve = new PVE(player1, [monster1, monster2]);
+
+function runBattles(battles: Battle[]): void {
+  battles.forEach((battle) => battle.fight());
+}
+
+export { player1, player2, player3, monster1, monster2, pve, runBattles };
